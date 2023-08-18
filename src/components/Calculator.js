defefa6835d2424019af314b9e53d8eac286d84d
Denys Hurtado <109555076+DenysHurtado7@users.noreply.github.com>
2023-08-18 01:09:15 -0400

    agregando funcionalidades a la calculadora

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -23,28 +23,49 @@ const Calculator = () => {
     setInput('');
   };
 
+  const handleScientificButtonClick = (value) => {
+    if (value === 'sin' || value === 'cos' || value === 'tan') {
+      setInput(`Math.${value}(Math.PI / 180 * ${input})`); // Convierte grados a radianes
+    } else if (value === '^') {
+      setInput(input + '**'); // Usa ** para representar la potenciación
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  const basicOperationButtons = ['+', '-', '*', '/'];
   const numberButtons = [];
   for (let i = 1; i <= 9; i++) {
     numberButtons.push(<Button key={i} value={i.toString()} onClick={handleButtonClick} />);
   }
+
+  const scientificOperationButtons = ['sin', 'cos', 'tan', 'log', '^'];
 
   return (
     <div className="calculator">
       <input className="form-control mb-3" type="text" value={input} readOnly />
       <div className="calculator-buttons">
         <div className="row">
-          <div className="col-md-9">
+          <div className="col-md-6">
             <div className="d-flex flex-wrap justify-content-between">
               {numberButtons}
               <Button value="0" onClick={handleButtonClick} />
+              <Button value="(" onClick={handleButtonClick} />
+              <Button value=")" onClick={handleButtonClick} />
             </div>
           </div>
           <div className="col-md-3">
             <div className="d-flex flex-wrap flex-column justify-content-between">
-              <Button value="+" onClick={handleButtonClick} />
-              <Button value="-" onClick={handleButtonClick} />
-              <Button value="*" onClick={handleButtonClick} />
-              <Button value="/" onClick={handleButtonClick} />
+              {basicOperationButtons.map((value) => (
+                <Button key={value} value={value} onClick={handleButtonClick} />
+              ))}
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="d-flex flex-wrap flex-column justify-content-between">
+              {scientificOperationButtons.map((value) => (
+                <Button key={value} value={value} onClick={() => handleScientificButtonClick(value)} />
+              ))}
               <Button value="=" onClick={handleCalculate} />
               <Button value="C" onClick={handleClear} />
             </div>
